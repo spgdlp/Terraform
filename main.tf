87f25fe45d13cd1c97d319ddd0e80f8439129671
spgdlp <spgdlp0@gmail.com>
2023-01-20 12:26:27 +0100

    S3

--- main.tf
+++ main.tf
@@ -8,30 +8,9 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration options
+  region = "eu-west-1"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "HelloWorld"
-  }
+resource "aws_s3_bucket" "tf_bucket_1" {
+  bucket = "my_tf_bucket_1"
 }
